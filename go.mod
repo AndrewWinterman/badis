e1b4214babed4bf54e77eec01dc04527494211bd
AndrewWinterman <andrew.winterman@outreach.io>
2026-05-01 07:56:57 -0700

    feat: add consistent hashing router

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 
 require (
 	github.com/armon/go-metrics v0.4.1 // indirect
+	github.com/buraksezer/consistent v0.10.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgraph-io/ristretto/v2 v2.2.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
---
+++ proxy/router.go
@@ -0,0 +1,52 @@
+package proxy
+
+import (
+	"github.com/buraksezer/consistent"
+	"github.com/cespare/xxhash/v2"
+)
+
+type hasher struct{}
+
+func (h hasher) Sum64(data []byte) uint64 {
+	return xxhash.Sum64(data)
+}
+
+type shard string
+
+func (s shard) String() string {
+	return string(s)
+}
+
+const (
+	defaultPartitionCount    = 271
+	defaultReplicationFactor = 20
+	defaultLoad              = 1.25
+)
+
+type Router struct {
+	ring *consistent.Consistent
+}
+
+func NewRouter(shardAddrs []string) *Router {
+	cfg := consistent.Config{
+		PartitionCount:    defaultPartitionCount,
+		ReplicationFactor: defaultReplicationFactor,
+		Load:              defaultLoad,
+		Hasher:            hasher{},
+	}
+	
+	ring := consistent.New(nil, cfg)
+	for _, addr := range shardAddrs {
+		ring.Add(shard(addr))
+	}
+
+	return &Router{ring: ring}
+}
+
+func (r *Router) LocateKey(key []byte) string {
+	member := r.ring.LocateKey(key)
+	if member == nil {
+		return ""
+	}
+	return member.String()
+}
